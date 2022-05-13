88cb7ebe117c4876cc0f3608d9dc63fa333b4f98
Dawid Janiak <dawid.janiak1@onet.pl>
2022-05-13 17:54:49 +0200

    Final Version

---
+++ Client/Gamas.cs
@@ -0,0 +1,11 @@
+﻿namespace Client
+{
+    public class Gamas
+    {
+        public int CarId { get; set; }
+        public string NameCar { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Client/Rozwoje.cs
@@ -0,0 +1,9 @@
+﻿namespace Client
+{
+    public class Rozwoje
+    {
+        public int RozwojId { get; set; }
+        public string Nick { get; set; }
+        public string Wiadomosc { get; set; }
+    }
+}
